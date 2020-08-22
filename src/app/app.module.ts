c4b79bd013f7b76420c9ceb8d74dc9b4a69b39ab
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-08-22 12:21:43 -0600

    Project section grid set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { HttpClientModule } from '@angular/common/http';
 import { EmbedVideo } from 'ngx-embed-video';
+import { MatGridListModule } from "@angular/material/grid-list"
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +17,7 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ImagePreloadDirective } from './image-preload.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,6 +35,8 @@ import { ImagePreloadDirective } from './image-preload.directive';
     AppRoutingModule,
     HttpClientModule,
     NgxYoutubePlayerModule.forRoot(),    
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
